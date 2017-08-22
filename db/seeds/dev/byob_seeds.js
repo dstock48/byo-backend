18d0d44a90c304f916829162382ec0a8f341ae1c
Joe Heitkamp <noetic97@gmail.com>
2017-08-22 17:44:34 -0600

    seeding almost there

--- db/seeds/dev/byob_seeds.js
+++ db/seeds/dev/byob_seeds.js
@@ -1,20 +1,32 @@
 const states = require('../../../states');
-// const resorts = require('./resorts')
+const resorts = require('../../../resorts')
 // const trails = require('./trails')
 
-exports.seed = function(knex, Promise) {
+exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   return knex('trails').del()
-    .then(() => knex('resorts')).del()
-    .then(() => knex('states')).del()
+    .then(() => knex('resorts').del())
+    .then(() => knex('states').del())
 
     .then(() => {
       // Inserts seed entries
       return Promise.all([
-        knex('states').insert({
-          states
-        }, 'id')
+        knex('states').insert(states,'*')
       ])
+      .then(statesArray => {
+        return Promise.all([
+          knex('resorts').insert(resorts.map((resort) => {
+            const thisState = statesArray.find((state) => {
+              console.log(state[0].state_name, 'stste');
+              return state[0].state_name.toLowerCase() === resort.state_name.toLowerCase()
+            });
+
+             console.log(resort.state_name);
+             console.log(this.state);
+            return Object.assign(resort, {states_id: parseInt(thisState.id)})
+          }))
+        ])
+      })
     })
     .catch(err => console.log('ERROR: ', err))
 };
