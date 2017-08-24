6ba362899f2012ec2d3e19d9c946f7af580867d7
Joe Heitkamp <noetic97@gmail.com>
2017-08-24 11:16:20 -0600

    add get methods for state resorts and resort trails

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
-const db = require('knex')(configuration)
+const db = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 
@@ -14,41 +15,44 @@ app.use(express.static('public'));
 
 app.get('/api/v1/states', (req, res) => {
   db('states').select()
-  .then(states => {
-    res.status(200).json(states);
-  })
-  .catch(error => {
-    res.status(500).json({ error });
-  })
-})
+    .then((states) => {
+      res.status(200).json(states);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.post('/api/v1/states', (req, res) => {
   const newState = req.body;
+  const requiredParameter = ['state_name', 'state_abbreviation'];
 
-  for (let requiredParameter of ['state_name', 'state_abbreviation']) {
-    if (!newState[requiredParameter]) {
+  requiredParameter.forEach((param) => {
+    if (!newState[param]) {
       return res.status(422).json({
-        error: `Missing required parameter ${requiredParameter}.`
-      })
-      if (newState.state_abbreviation !== 2) {
-        return res.status(422).json({
-          error: 'State abbreviations must be exactly 2 characters.'
-        })
-      }
+        error: `Missing required parameter ${param}.`,
+      });
     }
-  }
+    if (newState.state_abbreviation.length !== 2) {
+      return res.status(422).json({
+        error: 'State abbreviations must be exactly 2 characters.',
+      });
+    }
+    return null;
+  });
+
   newState.state_abbreviation = newState.state_abbreviation.toUpperCase();
   newState.state_name = newState.state_name.charAt(0).toUpperCase() +
-  newState.state_name.slice(1).toLowerCase()
+  newState.state_name.slice(1).toLowerCase();
 
   db('states').insert(newState, 'id')
-  .then(state => {
-    res.status(201).json(newState)
-  })
-  .catch(error => {
-    res.status(500).json({ error });
-  });
-})
+    .then(() => {
+      res.status(201).json(newState);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.patch('/api/v1/states/:id', (req, res) => {
   const { id } = req.params;
@@ -56,130 +60,151 @@ app.patch('/api/v1/states/:id', (req, res) => {
 
   if (patch.state_abbreviation.length !== 2) {
     return res.status(422).json({
-      error: 'State abbreviations must be exactly 2 characters.'
-    })
+      error: 'State abbreviations must be exactly 2 characters.',
+    });
   }
   if (patch.id) {
     return res.status(422).json({
-      error: 'You cannot change the ID.'
-    })
+      error: 'You cannot change the ID.',
+    });
   }
-  patch.state_abbreviation = patch.state_abbreviation.toUpperCase()
+  patch.state_abbreviation = patch.state_abbreviation.toUpperCase();
   patch.state_name = patch.state_name.charAt(0).toUpperCase() +
-  patch.state_name.slice(1).toLowerCase()
+  patch.state_name.slice(1).toLowerCase();
   db('states').where({ id }).update(patch)
-  .then(patched => {
-    res.status(201).json(patch)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+    .then(() => {
+      res.status(201).json(patch);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+  return null;
+});
 
 app.delete('/api/v1/states/:id', (req, res) => {
   const { id } = req.params;
   db('states').where({ id }).del()
-  .then(res => {
-    res.status(202)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+    .then(() => {
+      return res.status(204).json(`Success:  The state with ${id} has been successfully deleted!`);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
-app.get('/api/v1/states/:state_abbreviation', (req, res) => {
-  const { state_abbreviation } = req.params;
+app.get('/api/v1/states/:stateAbbreviation', (req, res) => {
+  const { stateAbbreviation } = req.params;
 
-  db('states').where('state_abbreviation', state_abbreviation.toUpperCase()).select()
-  .then(state => {
-    if (!state.length) {
+  db('states').where('state_abbreviation', stateAbbreviation.toUpperCase()).select()
+    .then((state) => {
+      if (!state.length) {
         return res.status(404).json({
-          error: `Could not find a state with a state abbreviation of ${state_abbreviation}`
+          error: `Could not find a state with a state abbreviation of ${stateAbbreviation}`,
         });
       }
-    res.status(200).json(state)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+      return res.status(200).json(state);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/states/:id/resorts', (req, res) => {
+  const { id } = req.params;
+
+  db('resorts').where({ states_id: id }).select()
+    .then((resort) => {
+      if (!resort.length) {
+        return res.status(404).json({
+          error: 'Could not find any resorts in this state.',
+        });
+      }
+      return res.status(200).json(resort);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.get('/api/v1/resorts', (req, res) => {
   db('resorts').select()
-  .then(resorts => {
-    res.status(200).json(resorts);
-  })
-  .catch(error => {
-    res.status(500).json({ error });
-  })
-})
+    .then((resorts) => {
+      res.status(200).json(resorts);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.get('/api/v1/resorts/:id', (req, res) => {
   const { id } = req.params;
 
   db('resorts').where({ id }).select()
-  .then(resort => {
-    if (!resort.length) {
+    .then((resort) => {
+      if (!resort.length) {
         return res.status(404).json({
-          error: `Could not find a resort with a resort with the id of ${id}`
+          error: `Could not find a resort with a resort with the id of ${id}`,
         });
       }
-    res.status(200).json(resort)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+      return res.status(200).json(resort);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.get('/api/v1/trails', (req, res) => {
   db('trails').select()
-  .then(trails => {
-    res.status(200).json(trails);
-  })
-  .catch(error => {
-    res.status(500).json({ error });
-  })
-})
+    .then((trails) => {
+      res.status(200).json(trails);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.post('/api/v1/trails', (req, res) => {
   const newTrail = req.body;
+  const requiredParameter = ['trail_name', 'trail_difficulty', 'resort_id'];
 
-  for (let requiredParameter of ['trail_name', 'trail_difficulty', 'resort_id']) {
-    if (!newTrail[requiredParameter]) {
+  requiredParameter.forEach((param) => {
+    if (!newTrail[param]) {
       return res.status(422).json({
-        error: `Missing required parameter ${requiredParameter}.`
-      })
+        error: `Missing required parameter ${param}.`,
+      });
     }
-  }
-  newTrail.trail_name = newTrail.trail_name.charAt(0).toUpperCase() +
-  newTrail.trail_name.slice(1).toLowerCase()
-
-  db('resorts').where(newTrail.resort_id).select('id')
-  db('trails').insert(newTrail)
-  .then(trail => {
-    res.status(201).json(newTrail)
-  })
-  .catch(error => {
-    res.status(500).json({ error });
+    return null;
   });
-})
+
+  newTrail.trail_name = newTrail.trail_name.charAt(0).toUpperCase() +
+  newTrail.trail_name.slice(1).toLowerCase();
+
+  db('resorts').where(newTrail.resort_id).select('id');
+  db('trails').insert(newTrail)
+    .then(() => {
+      res.status(201).json(newTrail);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.get('/api/v1/trails/:id', (req, res) => {
   const { id } = req.params;
 
   db('trails').where({ id }).select()
-  .then(trail => {
-    if (!trail.length) {
+    .then((trail) => {
+      if (!trail.length) {
         return res.status(404).json({
-          error: `Could not find a trail with a trail with the id of ${id}`
+          error: `Could not find a trail with a trail with the id of ${id}`,
         });
       }
-    res.status(200).json(trail)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+      return res.status(200).json(trail);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.patch('/api/v1/trails/:id', (req, res) => {
   const { id } = req.params;
@@ -187,31 +212,49 @@ app.patch('/api/v1/trails/:id', (req, res) => {
 
   if (patch.id) {
     return res.status(422).json({
-      error: 'You cannot change the ID.'
-    })
+      error: 'You cannot change the ID.',
+    });
   }
   db('trails').where({ id }).update(patch)
-  .then(patched => {
-    res.status(201).json(patch)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+    .then(() => {
+      res.status(201).json(patch);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+  return null;
+});
 
 app.delete('/api/v1/trails/:id', (req, res) => {
   const { id } = req.params;
   db('trails').where({ id }).del()
-  .then(res => {
-    res.status(202)
-  })
-  .catch(error => {
-    res.status(500).json({ error })
-  })
-})
+    .then(() => {
+      res.status(202);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/resorts/:id/trails', (req, res) => {
+  const { id } = req.params;
+
+  db('trails').where({ resort_id: id }).select()
+    .then((trail) => {
+      if (!trail.length) {
+        return res.status(404).json({
+          error: `There is no resort with an id of ${id}.`,
+        });
+      }
+      return res.status(200).json(trail);
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`BYO-Backend is running on http://localhost:${app.get('port')}`);
-})
+});
 
 module.exports = app;
