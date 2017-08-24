20de81d9d35165871be0e4d071686e9b50065602
Joe Heitkamp <noetic97@gmail.com>
2017-08-24 11:18:17 -0600

    lots of linter error cleanup

--- db/seeds/dev/byob_seeds.js
+++ db/seeds/dev/byob_seeds.js
@@ -3,7 +3,6 @@ const resorts = require('../../../resorts');
 // const trails = require('./trails')
 
 exports.seed = (knex, Promise) => {
-  // Deletes ALL existing entries
   return knex('trails').del()
     .then(() => knex('resorts').del())
     .then(() => knex('states').del())
@@ -13,7 +12,6 @@ exports.seed = (knex, Promise) => {
         .then((statesArray) => {
           return Promise.all([
             knex('resorts').insert(resorts.map((resort) => {
-              console.log(resort);
               const stateMatch = statesArray[0].find((state) => {
                 return state.state_name.toLowerCase() === resort.state_name.toLowerCase();
               });
--- ski-resort-scraper.js
+++ ski-resort-scraper.js
@@ -1,25 +1,19 @@
-/* eslint-disable */
-
 const fs = require('fs');
 const Nightmare = require('nightmare');
-var states = require('./states');
+const states = require('./states');
 
-const nightmare = Nightmare(); // 'show: true' makes the browser open so you can see the automation happening
-
+const nightmare = Nightmare();
 const result = [];
-
 const resortURLs = new Promise((res, rej) => {
   nightmare
     .goto('http://www.onthesnow.com/united-states/ski-resorts.html')
     .evaluate(() => {
-      const resortList = document.querySelectorAll('.resortList tr .name a');  //eslint-disable-line
-
+      const resortList = document.querySelectorAll('.resortList tr .name a');
       const resortLinks = [];
       for (let i = 0; i < 10; i += 1) {
         const link = resortList[i].href;
         resortLinks.push(link);
       }
-
       return resortLinks;
     })
     .end()
