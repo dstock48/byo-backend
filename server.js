bd79712ad50a3d1c81331eab046eb3d738d180af
Dave W <dviper@gmail.com>
2017-08-26 14:12:47 -0600

    add JWT validation to server, implement middleware on all endpoints

--- server.js
+++ server.js
@@ -3,12 +3,77 @@ const configuration = require('./knexfile')[environment];
 const db = require('knex')(configuration);
 const bodyParser = require('body-parser');
 const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+const secretKey = process.env.SECRET_KEY;
 const app = express();
+
+
+// /////////////////////////////////////////////////////////////////
+// MIDDLEWARE  /////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////
+
+const checkAuth = (request, response, next) => {
+  const token = request.body.token || request.query.token || request.headers.authorization;
+  let authorized;
+
+  if (!token) {
+    return response.status(403).json({ error: 'You must be authorized to hit this endpoint' });
+  }
+  try {
+    authorized = jwt.verify(token, secretKey);
+  } catch (err) {
+    response.status(403).json({ error: 'Invalid token' });
+  }
+  if (authorized) {
+    next();
+  }
+  return null;
+};
+
+const checkAdmin = (request, response, next) => {
+  const token = request.body.token || request.query.token || request.headers.authorization;
+  let authorized;
+
+  if (!token) {
+    return response.status(403).json({ error: 'You must be authorized to hit this endpoint' });
+  }
+  try {
+    authorized = jwt.verify(token, secretKey);
+  } catch (err) {
+    response.status(403).json({ error: 'Invalid token' });
+  }
+  if (authorized.admin) {
+    next();
+  } else {
+    response.status(403).json({ error: 'You must be an admin to hit this endpoint' });
+  }
+  return null;
+};
+
+
+// /////////////////////////////////////////////////////////////////
+// BIND MIDDLEWARE  ////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(checkAuth);
 app.use(express.static('public'));
+
+
+// /////////////////////////////////////////////////////////////////
+// JSON WEB TOKEN AUTH  ////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////
+
+app.post('/api/v1/auth', (req, res) => {
+  const { email, appName } = req.body;
+  const admin = email.endsWith('@turing.io');
+  const token = jwt.sign({ email, appName, admin }, secretKey, { expiresIn: '1y' });
+
+  res.status(201).json({ token });
+});
 
 
 // /////////////////////////////////////////////////////////////////
@@ -63,7 +128,7 @@ app.get('/api/v1/states/:id/resorts', (req, res) => {
 });
 
 // CREATE NEW STATE
-app.post('/api/v1/states', (req, res) => {
+app.post('/api/v1/states', checkAdmin, (req, res) => {
   const newState = req.body;
   const requiredParameter = ['state_name', 'state_abbreviation'];
 
@@ -88,6 +153,8 @@ app.post('/api/v1/states', (req, res) => {
   newState.state_abbreviation = newState.state_abbreviation.toUpperCase();
   newState.state_name = capitalizeState(newState.state_name);
 
+  if (newState.token) delete newState.token;
+
   db('states').insert(newState, 'id')
     .then(() => {
       res.status(201).json(newState);
@@ -98,7 +165,7 @@ app.post('/api/v1/states', (req, res) => {
 });
 
 // UPDATE STATE
-app.patch('/api/v1/states/:id', (req, res) => {
+app.patch('/api/v1/states/:id', checkAdmin, (req, res) => {
   const { id } = req.params;
   const updatedState = req.body;
 
@@ -124,6 +191,8 @@ app.patch('/api/v1/states/:id', (req, res) => {
     updatedState.state_name = capitalizeState(updatedState.state_name);
   }
 
+  if (updatedState.token) delete updatedState.token;
+
   db('states').where({ id }).update(updatedState)
     .then(() => res.status(200).json(updatedState))
     .catch(error => res.status(500).json({ error }));
@@ -131,7 +200,7 @@ app.patch('/api/v1/states/:id', (req, res) => {
 });
 
 // DELETE STATE
-app.delete('/api/v1/states/:id', (req, res) => {
+app.delete('/api/v1/states/:id', checkAdmin, (req, res) => {
   const { id } = req.params;
   db('states').where({ id }).del().returning('*')
     .then((state) => {
@@ -210,7 +279,7 @@ app.get('/api/v1/resorts/:id/trails', (req, res) => {
 });
 
 // CREATE NEW RESORT
-app.post('/api/v1/resorts', (req, res) => {
+app.post('/api/v1/resorts', checkAdmin, (req, res) => {
   const newResort = req.body;
 
   const requiredParams = [
@@ -236,6 +305,8 @@ app.post('/api/v1/resorts', (req, res) => {
     return null;
   });
 
+  if (newResort.token) delete newResort.token;
+
   db('resorts').insert(newResort, '*')
     .then((resort) => {
       res.status(201).json(resort[0]);
@@ -246,7 +317,7 @@ app.post('/api/v1/resorts', (req, res) => {
 });
 
 // UPDATE RESORT
-app.patch('/api/v1/resorts/:id', (req, res) => {
+app.patch('/api/v1/resorts/:id', checkAdmin, (req, res) => {
   const updatedResort = req.body;
   const { id } = req.params;
 
@@ -255,6 +326,8 @@ app.patch('/api/v1/resorts/:id', (req, res) => {
       error: 'You cannot change the ID.',
     });
   }
+
+  if (updatedResort.token) delete updatedResort.token;
 
   db('resorts').where('id', id).update(updatedResort, '*')
     .then((resort) => {
@@ -270,7 +343,7 @@ app.patch('/api/v1/resorts/:id', (req, res) => {
 });
 
 // DELETE RESORT
-app.delete('/api/v1/resorts/:id', (req, res) => {
+app.delete('/api/v1/resorts/:id', checkAdmin, (req, res) => {
   const { id } = req.params;
 
   db('resorts').where('id', id).del().returning('*')
@@ -323,7 +396,7 @@ app.get('/api/v1/trails/:id', (req, res) => {
 });
 
 // CREATE NEW TRAIL
-app.post('/api/v1/trails', (req, res) => {
+app.post('/api/v1/trails', checkAdmin, (req, res) => {
   const newTrail = req.body;
   const requiredParameter = ['trail_name', 'trail_difficulty', 'resort_id'];
 
@@ -339,6 +412,8 @@ app.post('/api/v1/trails', (req, res) => {
   newTrail.trail_name = newTrail.trail_name.charAt(0).toUpperCase() +
   newTrail.trail_name.slice(1).toLowerCase();
 
+  if (newTrail.token) delete newTrail.token;
+
   db('resorts').where(newTrail.resort_id).select('id');
   db('trails').insert(newTrail)
     .then(() => {
@@ -350,7 +425,7 @@ app.post('/api/v1/trails', (req, res) => {
 });
 
 // UPDATE TRAIL
-app.patch('/api/v1/trails/:id', (req, res) => {
+app.patch('/api/v1/trails/:id', checkAdmin, (req, res) => {
   const { id } = req.params;
   const updatedTrail = req.body;
 
@@ -359,6 +434,9 @@ app.patch('/api/v1/trails/:id', (req, res) => {
       error: 'You cannot change the ID.',
     });
   }
+
+  if (updatedTrail.token) delete updatedTrail.token;
+
   db('trails').where({ id }).update(updatedTrail)
     .then(() => {
       res.status(201).json(updatedTrail);
@@ -370,7 +448,7 @@ app.patch('/api/v1/trails/:id', (req, res) => {
 });
 
 // DELETE TRAIL
-app.delete('/api/v1/trails/:id', (req, res) => {
+app.delete('/api/v1/trails/:id', checkAdmin, (req, res) => {
   const { id } = req.params;
   db('trails').where({ id }).del().returning('*')
     .then((trail) => {
